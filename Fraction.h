c7798cbdb82713e5f41aba40783488130b72f9b5
fartb <fartbreather228@gmail.com>
2025-01-03 00:02:19 +0300

    Добавлен выброс иключения в модификатор знаменателя в классе, срабатывающий если присваеваемое значение равно 0

--- Fraction.h
+++ Fraction.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "Exception.h"
 using namespace std;
 
 namespace user_fraction
@@ -15,6 +16,10 @@ namespace user_fraction
 		}
 		void set_den(int den1)
 		{
+			if (den1 == 0)
+			{
+				throw InvalidArgument("\n\nDenomenator can't possibly be 0!\n\n");
+			}
 			den = den1;
 		}
 		int get_num()
