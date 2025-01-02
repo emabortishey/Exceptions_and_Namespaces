1ee2b5beb891c7a9982c966347c61981c4a26543
fartb <fartbreather228@gmail.com>
2025-01-02 23:34:13 +0300

    создана иерархия классов исключений (задание 2 встречи 22)

--- Exception.h
+++ Exception.h
@@ -27,3 +27,102 @@ public:
 
 	virtual string what() override { return msg; }
 };
+
+class FileError : public Exception
+{
+	string msg;
+public:
+	explicit FileError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class BadCast : public Exception
+{
+	string msg;
+public:
+	explicit BadCast(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class RunTimeError : public Exception
+{
+	string msg;
+public:
+	explicit RunTimeError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class OverFlowError : public RunTimeError
+{
+	string msg;
+public:
+	explicit OverFlowError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class UnderFlowError : public RunTimeError
+{
+	string msg;
+public:
+	explicit UnderFlowError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class LogicError : public Exception
+{
+	string msg;
+public:
+	explicit LogicError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class InvalidArgument : public LogicError
+{
+	string msg;
+public:
+	explicit InvalidArgument(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class OutOfRange : public LogicError
+{
+	string msg;
+public:
+	explicit OutOfRange(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class FileError : public Exception
+{
+	string msg;
+public:
+	explicit FileError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class OpenFileError : public FileError
+{
+	string msg;
+public:
+	explicit OpenFileError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
+
+class CloseFileError : public FileError
+{
+	string msg;
+public:
+	explicit CloseFileError(string msg_P) : msg{ msg_P } { };
+
+	virtual string what() override { return msg; }
+};
